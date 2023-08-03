980b8dbb20baef52ed42bb9e53086c132df29565
Toma Alexandru <a_tomaaa@yahoo.com>
2023-08-03 11:41:19 +0300

    Python updated

--- Scraping/PYTHON/scraping.py
+++ Scraping/PYTHON/scraping.py
@@ -5,50 +5,13 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-def extract_description_from_url(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Check for any request errors
-
-        soup = BeautifulSoup(response.content, 'html.parser')
-        print("get the description")
-        description_element = soup.find('div', {'class': 'css-1wekrze e1lbnp621'})
-        print("====")
-        print(description_element)
-        return extract_text_from_html(description_element)
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred while requesting the URL: {e}")
-        return None
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
-def extract_text_from_html(html_text):
-    try:
-        soup = BeautifulSoup(html_text, 'html.parser')
-
-        description_div = soup.find('div', {'data-cy': 'adPageAdDescription'})
-        if description_div:
-            paragraphs = description_div.find_all('p')
-            extracted_text = '\n'.join(paragraph.get_text(strip=True) for paragraph in paragraphs)
-            return extracted_text
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-    return None
-
 mongo_uri = os.getenv("MONGO_URI")
 db_name = os.getenv("DB_NAME")
 collection_name = os.getenv("COLLECTION_NAME")
-olx_url = os.getenv("OLX_URL")
-
-#the url to scrape==>>
-url_to_scrape = olx_url
-response = requests.get(url_to_scrape)
+olx_url = "https://www.olx.ro/imobiliare/apartamente-garsoniere-de-vanzare/2-camere/?currency=EUR&view=grid"
 
 try:
-    # Connecting to MongoDB
+    # Connecting to MongoDB try separat!
     client = pymongo.MongoClient(mongo_uri)
     db = client[db_name]
     collection = db[collection_name]
